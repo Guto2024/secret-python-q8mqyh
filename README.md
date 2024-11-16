93d1a53015adedd48795621e23a4da322f2f4e90
Guto2024 <perrreira2045@gmail.com>
2024-11-16 19:17:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# secret-python-q8mqyh
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Guto2024/secret-python-q8mqyh)
---
+++ main.py
@@ -0,0 +1,6 @@
+largura = input ("Digite sua largura: ")
+altura = input ("digite sua altura: ")
+
+area = largura + altura
+
+print(f"a area do retãngulo é (area) m2.")
